5f91cb942a9bb4b126b315a048156563c0f18389
castilho-maker <elizarusczak@gmail.com>
2025-06-27 12:01:10 -0300

    Primeiro commit

---
+++ pizzas.js
@@ -0,0 +1,34 @@
+let cardapio = [
+
+    {
+        foto: "imgs/pizza1.jpg",
+        nome: "Mista",
+        ingredientes: ["Tomate", "Cebola", "Mussarela", "Azeitona"],
+        valor: 35.00
+    },
+    {
+        foto: "imgs/pizza2.jpg",
+        nome: "Mista",
+        ingredientes: ["Tomate", "Cebola", "Mussarela", "Azeitona"],
+        valor: 35.00
+    },
+    {
+        foto: "imgs/pizza3.jpg",
+        nome: "Mista",
+        ingredientes: ["calabresa", "tomate", "mussarela", "brocólis"],
+        valor: 35.00
+    },
+    {
+        foto: "imgs/pizza4.jpg",
+        nome: "Calabresa",
+        ingredientes: ["linguiça"],
+        valor: 35.00
+    },
+
+    {
+        foto: "imgs/pizza5.jpg",
+        nome: "Frango",
+        ingredientes: ["frango", "Catupiry"],
+        valor: 35.00
+    }
+];
