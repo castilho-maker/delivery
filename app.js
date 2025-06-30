db945233a80e7a3bebcc64eaf184e2440ff5b7c3
castilho-maker <elizarusczak@gmail.com>
2025-06-30 11:53:28 -0300

    AlteradoDelivery

---
+++ app.js
@@ -0,0 +1,73 @@
+let total = 0;
+const cardapioUl = document.getElementById('cardapio');//<ul id="cardapio"></ul>
+const modal = document.getElementById('modal');
+const formCadastro = document.getElementById('cadastraPizza');
+const adicionarPizza = document.getElementById('adicionarPizza');
+const carrinhoUl = document.getElementById('carrinho');//<ul id="carrinho"></ul>       
+adicionarPizza.addEventListener('click', () => {
+    modal.open = true;
+});
+formCadastro.addEventListener('submit', (evento) => {
+    //para o evento padrão do formulario
+    evento.preventDefault();
+    let nome = document.getElementById('nome').value
+    let foto = document.getElementById('foto').value
+    let valor = parseFloat(document.getElementById('valor').value)
+    let itemIngredientes = document.getElementById('ingredientes').value
+    let ingredientes = itemIngredientes.split(',');
+    console.log(itemIngredientes);
+    console.log(ingredientes);
+
+
+    let novaPizza = {
+        foto: foto,
+        nome: nome,
+        ingredientes: [ingredientes],
+        valor: valor
+    };
+    cardapio.push(novaPizza);
+    carrinhoUl.innerHTML = "";
+    exibirCardapio();
+});
+    const fechaModal = document.getElementById('fecharmodal');
+    ////////////////////////////////////////////
+    fecharModal.addEventListener('click', () => {
+        modal.open = false;
+    });
+    /////////////////////////////////////////////////////////
+    let carrinho = [];
+    //zera a lista do cardápio
+    cardapioUl.innerHTML = "";
+    //repetição para exibir a lista de pizzas no cardapio (ul)
+
+    //================= EVENTO ABRIR CARRINHO ===================
+    //Seleciona o botão no menu para abrir o carrinho
+    const btnAbrirCarrinho = document.getElementById('abrirCarrinho');
+    //seleciona a DIV "checkout" para manipular o display (block ou none)
+    const checkout = document.getElementById('checkout');
+    btnAbrirCarrinho.addEventListener('click', () => {
+        //altera o display da div para 'block'
+        checkout.style.display = 'block';
+    });
+    //================= EVENTO FECHAR CARRINHO ===================
+    const fecharCarrinho = document.getElementById('fecharCarrinho');
+    fecharCarrinho.addEventListener('click', () => {
+        //altera o display da div para 'none'
+        checkout.style.display = 'none';
+    });
+    carrinhoUl.innerHTML = "<h4>🥹 Seu carrinho está vazio 🍕</h4>";
+    //================= ATUALIZAR CARRINHO ===================
+
+    //--------- Exibe o valor total a pagar --------
+    const divTotal = document.querySelector('.total');
+    divTotal.innerHTML = `TOTAL R$ ${total.toFixed(2)}`;
+    //fecha função atualizar
+
+    function removerCarrinho(indice) {
+        //remove o indide clicado 
+        //função para array. splice(onde começa a remover, quantidade a remover)
+        carrinho.splice(indice, 1);
+        //atualiza o carrinho após a remoção
+        atualizarCarrinho();
+    }
+    exibirCardapio();
--- pizzas.js
+++ pizzas.js
@@ -1,34 +1,27 @@
+
 let cardapio = [
-
     {
-        foto: "imgs/pizza1.jpg",
-        nome: "Mista",
-        ingredientes: ["Tomate", "Cebola", "Mussarela", "Azeitona"],
-        valor: 35.00
-    },
-    {
-        foto: "imgs/pizza2.jpg",
-        nome: "Mista",
-        ingredientes: ["Tomate", "Cebola", "Mussarela", "Azeitona"],
-        valor: 35.00
-    },
-    {
-        foto: "imgs/pizza3.jpg",
-        nome: "Mista",
-        ingredientes: ["calabresa", "tomate", "mussarela", "brocólis"],
-        valor: 35.00
-    },
-    {
-        foto: "imgs/pizza4.jpg",
+        foto: "imgs/Pizza4.jpg",
         nome: "Calabresa",
-        ingredientes: ["linguiça"],
+        ingredientes: ["Calabresa", "Cebola", "Muçarela"],
         valor: 35.00
     },
-
     {
-        foto: "imgs/pizza5.jpg",
-        nome: "Frango",
-        ingredientes: ["frango", "Catupiry"],
-        valor: 35.00
+        foto: "imgs/Pizza2.jpg",
+        nome: "Muçarela",
+        ingredientes: ["Beringela", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "imgs/Pizza3.jpg",
+        nome: "Mista de Vegetais",
+        ingredientes: ["Beringela", "Abobrinha", "Pimentão", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "imgs/Pizza4.jpg",
+        nome: "Frango com Catupiry",
+        ingredientes: ["Frango", "Catupiry", "Muçarela"],
+        valor: 40.00
     }
 ];
